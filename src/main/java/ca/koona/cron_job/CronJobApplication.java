24a809184c0606e18e64d8eb8492db2948c3865c
Steve KOUNA <stevekouna@gmail.com>
2026-03-18 09:06:18 -0400

    init repot

---
+++ src/main/java/ca/koona/cron_job/CronJobApplication.java
@@ -0,0 +1,13 @@
+package ca.koona.cron_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/koona/cron_job/CronJobApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.koona.cron_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
